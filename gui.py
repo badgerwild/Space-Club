fee49cc33e85b3c8855b712d4ab120c3cba791d6
badgerwild <42945217+badgerwild@users.noreply.github.com>
2018-10-11 22:22:33 -0400

    modified to merge the serial communication with the web app. html has the buttons. css is terrible.

--- gui.py
+++ gui.py
@@ -1,11 +1,23 @@
-from flask import Flask,render_template
+import serial
+from flask import render_template, request
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/gui')
+@app.route('/gui', methods=(['GET', 'POST']))
 def hello_world():
+    if request.method =='POST':
+        request.form['submit']
+        arduino_serial = serial.Serial('COM3', 9600, timeout=1)  # Create Serial port object called arduinoSerialData
+        if request.form['submit']== 'Blink Fast':  # if the entered data is blink fast
+            print(b'1')
+            arduino_serial.write(b'1')  # send 1 to arduino
+        elif request.form['submit']=='Blink Slow':
+            arduino_serial.write(b'0')  # send 0 to arduino
+            print(b'1')
+
     return render_template('gui.html')
 
 
