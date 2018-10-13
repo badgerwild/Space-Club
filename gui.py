a85ed0998a7c59f4be23f2fa65bc6d25d840ee0e
badgerwild <42945217+badgerwild@users.noreply.github.com>
2018-10-13 19:42:11 -0400

    Moved the open serial port out of the def and out of the @ route. works now.

--- gui.py
+++ gui.py
@@ -4,19 +4,23 @@ from flask import Flask
 
 app = Flask(__name__)
 
+arduino_serial = serial.Serial('COM3', 9600, timeout=1)  # Create Serial port object called arduinoSerialData
 
 @app.route('/')
+
 @app.route('/gui', methods=(['GET', 'POST']))
+
 def hello_world():
+
     if request.method =='POST':
         request.form['submit']
-        arduino_serial = serial.Serial('COM3', 9600, timeout=1)  # Create Serial port object called arduinoSerialData
         if request.form['submit']== 'Blink Fast':  # if the entered data is blink fast
-            print(b'1')
-            arduino_serial.write(b'1')  # send 1 to arduino
+            arduino_serial.write(str.encode(str('1')))
+            print('1')
         elif request.form['submit']=='Blink Slow':
-            arduino_serial.write(b'0')  # send 0 to arduino
-            print(b'1')
+            # send 1 to arduino
+            arduino_serial.write(str.encode(str(0)))  # send 0 to arduino
+            print('0')
 
     return render_template('gui.html')
 
