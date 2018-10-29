97db76236db9a196cfbbd3cbdb16949ee22b1a7a
badgerwild <42945217+badgerwild@users.noreply.github.com>
2018-10-28 22:07:46 -0400

    Added an off button

--- gui.py
+++ gui.py
@@ -4,23 +4,26 @@ from flask import Flask
 
 app = Flask(__name__)
 
-arduino_serial = serial.Serial('COM3', 9600, timeout=1)  # Create Serial port object called arduinoSerialData
+arduino_serial = serial.Serial('COM4', 9600, timeout=1)  # Create Serial port object called arduinoSerialData
 
 @app.route('/')
 
 @app.route('/gui', methods=(['GET', 'POST']))
 
-def hello_world():
+def Blink_light():
 
     if request.method =='POST':
         request.form['submit']
         if request.form['submit']== 'Blink Fast':  # if the entered data is blink fast
-            arduino_serial.write(str.encode(str('1')))
+            arduino_serial.write(str.encode(str(1)))
             print('1')
         elif request.form['submit']=='Blink Slow':
             # send 1 to arduino
             arduino_serial.write(str.encode(str(0)))  # send 0 to arduino
             print('0')
+        elif request.form['submit']=='Turn Off':
+            arduino_serial.write(str.encode(str(2)))  # send 2 to arduino
+            print('2')
 
     return render_template('gui.html')
 
