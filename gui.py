55c01cecb545169f3863cc89d676d89d02af1390
badgerwild <42945217+badgerwild@users.noreply.github.com>
2018-10-29 09:59:24 -0400

    styled the buttons, added flex containers to template

--- gui.py
+++ gui.py
@@ -14,15 +14,15 @@ def Blink_light():
 
     if request.method =='POST':
         request.form['submit']
-        if request.form['submit']== 'Blink Fast':  # if the entered data is blink fast
+        if request.form['submit']== 'Blink Fast':  # sends a 1 to arduino, makes it blink fast
             arduino_serial.write(str.encode(str(1)))
             print('1')
         elif request.form['submit']=='Blink Slow':
             # send 1 to arduino
-            arduino_serial.write(str.encode(str(0)))  # send 0 to arduino
+            arduino_serial.write(str.encode(str(0)))  # sends 0 to arduino, makes it blink slower
             print('0')
         elif request.form['submit']=='Turn Off':
-            arduino_serial.write(str.encode(str(2)))  # send 2 to arduino
+            arduino_serial.write(str.encode(str(2)))  # send 2 to arduino, turns off led
             print('2')
 
     return render_template('gui.html')
